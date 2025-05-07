b7d06de1dc55d7313bcb62d44f98a332f96efce8
faizanhassan-softpers <faizanhassan@team.softpers.com>
2025-05-07 09:19:11 +0000

    test script update

--- LLaVA_Bifrost/test.py
+++ LLaVA_Bifrost/test.py
@@ -11,10 +11,22 @@ tokenizer, model, image_processor, context_len = load_pretrained_model(
 )
 
 model_path = "liuhaotian/llava-v1.5-7b"
-prompt = """ 
-    Place the bag on the chair of the image. Return: 
-    The bounding box of the object in [x_min, y_min, x_max, y_max] format. 
-    The depth value at the center point of the image.
+# prompt = """ 
+#     Place the bag on the chair of the image. Return: 
+#     The bounding box of the object in [x, y, w, h] in the range of [0, 1] format. 
+#     The depth value at the center point of the image.
+# """
+prompt = """
+Place a medium-sized bag naturally on the chair in the image.
+Return only the following:
+
+The bounding box of the object in normalized [x, y, w, h] format (values between 0 and 1).
+
+Ensure x + w ≤ 1 and y + h ≤ 1.
+
+The depth value at the center of the image.
+Format the response exactly as:
+[x, y, w, h], [x, y]
 """
 image_file = "/home/ec2-user/dev/Bifrost/Main_Bifrost/examples/TEST/Input/background.jpg"
 
